083e1cdb07039dde95e2c9d65819a7cdef746560
anshultyag <you@example.com>
2022-01-09 23:10:00 +0530

    Added: Ability to create a contacts in Address book

---
+++ src/com/company/Person.java
@@ -0,0 +1,41 @@
+package com.company;
+
+public class Person {
+    private String fname;
+    private String lname;
+    private String address;
+
+    public Person() {
+        this.fname = fname;
+        this.lname = lname;
+        this.address = address;
+    }
+
+    public Person(String fname, String lname, String address) {
+        this.fname = fname;
+        this.lname = lname;
+        this.address = address;
+    }
+
+    public String getName() {
+        return fname;
+    }
+    public void setName(String name) {
+        this.fname = fname;
+    }
+
+
+    public String getLname() {
+        return lname;
+    }
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
